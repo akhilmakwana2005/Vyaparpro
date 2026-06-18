857214643898cb256acd2500ce616a532ad35ceb
akhilmakwana2005 <akhilmakwana745@gmail.com>
2026-06-18 17:00:46 +0530

    Fix production axios base URL

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import { Toaster } from 'react-hot-toast';
 import axios from 'axios';
 
 axios.defaults.withCredentials = true;
-
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || '';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
